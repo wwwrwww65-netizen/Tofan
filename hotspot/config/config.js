914fd811ee719dd269e69a6be29088ab4fcb14d2
wwwrwww65-netizen <wwwrwww65@gmail.com>
2026-09-22 00:59:39 +0300

    اضافه شبكه الجامعه

---
+++ hotspot/config/config.js
@@ -0,0 +1,109 @@
+﻿Config({
+    "network-name": "الجامعة",
+    "service-number": "770776483",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "passwordAsUser",
+    "news-line": "...:::: اهلاً بكم على شبكتنا اللاسلكية تتميز شبكتنا بتقديم مزايا مختلفة لمشتركيها الاعزاء منها السرعة في التصفح ولعب الألعاب والبث المباشر للمباريات ايضاً نقدم خدمة توصيل الانترنت الى المنازل لسنا الوحيدون ولكن نسعى لتقديم خدمة ممتازة على مدار 24 ساعة::::...",
+    "input-type": "text",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+        {
+            "price": "100 ريال ",
+            "time": "4 ساعات",
+            "transfer": "400 ميجا",
+            "validity": "4 يوم",
+        },
+
+        {
+            "price": "200 ريال",
+            "time": "12 ساعة",
+            "transfer": "700 ميجا",
+            "validity": "10 يوم",
+        },
+  
+        {
+            "price": "250 ريال",
+            "time": "15 ساعة",
+            "transfer": "1 جيجا",
+            "validity": "15 يوم",
+        },
+		
+		{
+            "price": "500 ريال",
+            "time": "50 ساعة",
+            "transfer": "2 جيجا",
+            "validity": "20 يوم",
+        },
+		
+		{
+            "price": "",
+            "time": "بدون تحديد",
+            "transfer": "25 جيجا",
+            "validity": "30 يوم",
+        },
+		
+		{
+            "price": "",
+            "time": "بدون تحديد",
+            "transfer": "30 جيجا",
+            "validity": "30 يوم",
+        },
+
+        {
+            "price": "",
+            "time": "بدون تحديد",
+            "transfer": "50 جيجا",
+            "validity": "30 يوم",
+        },
+
+        {
+            "price": "",
+            "time": "بدون تحديد",
+            "transfer": "75 جيجا",
+            "validity": "30 يوم",
+        },
+		
+		{
+            "price": "",
+            "time": "بدون تحديد",
+            "transfer": "100 جيجا",
+            "validity": "30 يوم",
+        },
+
+    ],
+    "sell-points": [
+        { "name": "المركز الرئيسي مركز الجامعة" },
+        { "name": "جميع البقائل المجاوة للشبكة" },
+        { "name": "" },
+
+    ]
+})
+
+
+
